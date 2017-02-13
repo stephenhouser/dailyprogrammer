be6eaee3679f22a9d737c19011d02ed3ea96acf5
Stephen Houser <stephenhouser@gmail.com>
2017-02-13 13:59:28 -0500

    adventure complete with houser.story

--- lib/strings.c
+++ lib/strings.c
@@ -30,3 +30,47 @@ int strip(char *string) {
 
     return count;
 }
+
+/* Trim leading spaces in string */
+int ltrim(char *string) {
+    char *s = string;
+    char *d = string;
+    int count = 0;
+
+    /* don't copy anything if the first char is non-space */
+    if (isspace(*s)) {
+        /* advance s to first non-space */
+        while(*s != '\0' && isspace(*s)) {
+            s++;
+            count++;
+        }
+
+        /* copy non-spaces to start of string */
+        while(*s != '\0') {
+            *d++ = *s++;
+        }
+
+        *d = *s; // '\0'
+    }
+
+    return count;    
+}
+
+/* Trim trailing spaces in string */
+int rtrim(char *string) {
+    char *s = string + strlen(string) - 1;
+    int count = 0;
+
+    while(isspace(*s)) {
+        *s-- = '\0';
+        count++;
+    }
+
+    return count;    
+}
+
+/* Trim leading and trailing spaces from string */
+int trim(char *string) {
+    int count = rtrim(string) + ltrim(string);
+    return count;
+}
--- lib/strings.h
+++ lib/strings.h
@@ -5,3 +5,10 @@ extern int strip(char *string);
 
 /* Strip spaces from string (in-place). */
 extern int chomp(char *string);
+
+/* Trim leading spaces in string */
+int ltrim(char *string);
+/* Trim trailing spaces in string */
+int rtrim(char *string);
+/* Trim leading and trailing spaces from string */
+int trim(char *string);
